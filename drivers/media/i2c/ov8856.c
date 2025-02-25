c834b3007b16a9c67e4b73f1559037c35ffe519b
Vitalii Skorkin <nikroksm@mail.ru>
2025-02-25 18:45:32 +0500

    media: i2c: ov8856: comment 3280x2464 2 lane mode
    Signed-off-by: Vitalii Skorkin <nikroksm@mail.ru>

--- drivers/media/i2c/ov8856.c
+++ drivers/media/i2c/ov8856.c
@@ -1473,7 +1473,7 @@ static const struct ov8856_lane_cfg lane_cfg_2 = {
 			.regs = mipi_data_rate_lane_2.regs_1,
 		}
 	}},
-	{{
+	{/*{
 		.width = 3280,
 		.height = 2464,
 		.hts = 1928,
@@ -1487,7 +1487,7 @@ static const struct ov8856_lane_cfg lane_cfg_2 = {
 		.link_freq_index = 0,
 		.data_lanes = 2,
 		.default_mbus_index = OV8856_MEDIA_BUS_FMT_SGRBG10_1X10,
-	},
+	},*/
 	{
 		.width = 1640,
 		.height = 1232,
