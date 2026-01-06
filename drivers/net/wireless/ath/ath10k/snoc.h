3c2f085819bfa59f549854f89bf02cd15a6cdcbc
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-01-06 03:01:14 +0200

    FROMLIST: wifi: ath10k: snoc: support powering on the device via pwrseq
    The WCN39xx family of WiFi/BT chips incorporates a simple PMU, spreading
    voltages over internal rails. Implement support for using powersequencer
    for this family of ATH10k devices in addition to using regulators.
    
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath10k/snoc.h
+++ drivers/net/wireless/ath/ath10k/snoc.h
@@ -53,6 +53,7 @@ enum ath10k_snoc_flags {
 };
 
 struct clk_bulk_data;
+struct pwrseq_desc;
 struct regulator_bulk_data;
 
 struct ath10k_snoc {
@@ -73,6 +74,7 @@ struct ath10k_snoc {
 	struct ath10k_snoc_ce_irq ce_irqs[CE_COUNT_MAX];
 	struct ath10k_ce ce;
 	struct timer_list rx_post_retry;
+	struct pwrseq_desc *pwrseq;
 	struct regulator_bulk_data *vregs;
 	size_t num_vregs;
 	struct clk_bulk_data *clks;
